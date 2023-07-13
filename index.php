c378855c1d9fac43993c7c46c1e2e33c61651782
Fran Grados <gradosberna@gmail.com>
2023-07-12 19:32:49 -0500

    Primer archivo php1.3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Git Prueba</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' href='css/style.css'>
     <script src='main.js'></script>
 </head>
 <body>
