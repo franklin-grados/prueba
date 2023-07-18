d187d15ee0743f85fe0bb41fe069764b597c9758
Fran Grados <gradosberna@gmail.com>
2023-07-18 08:21:04 -0500

    Primer archivo php1.cuerpo1

--- index.php
+++ index.php
@@ -11,9 +11,6 @@
     <script src='main.js'></script>
 </head>
 <body>
-    <!-- <h1>Hola mundo</h1>
-    <p>Soy fran Grados mi edad 25</p>
-    <p>vivo en tingo maria</p> -->
 
     <h1 class="title">este es un titulo interesante</h1>
     <p>trata de ramas o branches de git</p>
